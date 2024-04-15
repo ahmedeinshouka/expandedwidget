a91ca1571b4f51a24b187b3e3b388087e4dca876
ahmedeinshouka <76920911+ahmedeinshouka@users.noreply.github.com>
2024-04-15 03:20:15 +0200

    commit

--- lib/center.dart
+++ lib/center.dart
@@ -28,138 +28,59 @@ class _newappState extends State<newapp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-        appBar: AppBar(
-          leading: IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.menu,
-                color: Color.fromARGB(255, 0, 0, 0),
-              )),
-          elevation: 10,
-          title: Text(
-            "FACEBOOK",
-            style: TextStyle(
-                color: Color.fromARGB(255, 29, 7, 150),
-                fontWeight: FontWeight.bold),
-            textAlign: TextAlign.left,
-          ),
-          centerTitle: true,
-          actions: [
-            IconButton(onPressed: () {}, icon: Icon(Icons.message)),
-            IconButton(onPressed: () {}, icon: Icon(Icons.search))
-          ],
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.menu,
+              color: Color.fromARGB(255, 0, 0, 0),
+            )),
+        elevation: 10,
+        title: Text(
+          "FACEBOOK",
+          style: TextStyle(
+              color: Color.fromARGB(255, 29, 7, 150),
+              fontWeight: FontWeight.bold),
+          textAlign: TextAlign.left,
         ),
-        body: 
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  child: Text(
-                    "hello",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        color: Color.fromARGB(255, 255, 255, 255), fontSize: 44),
-                  ),
-                  alignment: Alignment.center,
-                  height: 322,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 0, 0, 0),
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  margin: EdgeInsets.only(bottom: 22),
-                ),
+        centerTitle: true,
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.message)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.search))
+        ],
+      ),
+      body: Center(
+          child: Container(
+        width: 300,
+        height: 300,
+        color: Colors.blue,
+        child: Row(
+          children: [
+            Container(
+              child: Text(
+                "c4a-courses",
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
               ),
-              SizedBox(
-                height: 20,
+              height: 100,
+              width: 100,
+              color: Colors.green,
+              alignment: Alignment.center,
+            ),
+            Container(
+              child: Text(
+                "c4a-courses",
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
               ),
-              SingleChildScrollView(
-                child: Container(
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Row(
-                      children: [
-                        Container(
-                          child: Text(
-                            "hello",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Color.fromARGB(255, 255, 255, 255),
-                            ),
-                          ),
-                          alignment: Alignment.center,
-                          height: 200,
-                          width: 200,
-                          decoration: BoxDecoration(
-                            color: Color.fromARGB(255, 0, 0, 0),
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                          margin: EdgeInsets.fromLTRB(20, 0, 20, 100),
-                        ),
-                        Container(
-                          child: Text(
-                            "hello",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Color.fromARGB(255, 255, 255, 255),
-                            ),
-                          ),
-                          alignment: Alignment.center,
-                          height: 200,
-                          width: 200,
-                          decoration: BoxDecoration(
-                            color: Color.fromARGB(255, 0, 0, 0),
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                          margin: EdgeInsets.fromLTRB(0, 0, 20, 100),
-                        ),
-                        Container(
-                          child: Text(
-                            "hello",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Color.fromARGB(255, 255, 255, 255),
-                            ),
-                          ),
-                          alignment: Alignment.center,
-                          height: 200,
-                          width: 200,
-                          decoration: BoxDecoration(
-                            color: Color.fromARGB(255, 0, 0, 0),
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-                          margin: EdgeInsets.fromLTRB(0, 0, 20, 100),
-                        )
-                      ],
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                    ),
-                  ),
-                  color: Color.fromARGB(255, 0, 29, 157),
-                  alignment: Alignment.center,
-                  margin: EdgeInsets.only(bottom: 20),
-                ),
-                scrollDirection: Axis.horizontal,
-              ),
-              Container(
-                child: Text(
-                  "hello",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Color.fromARGB(255, 255, 255, 255), fontSize: 44),
-                ),
-                alignment: Alignment.center,
-                height: 322,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 0, 0, 0),
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                margin: EdgeInsets.only(bottom: 22),
-              )
-            ],
-          ),
-        ));
+              height: 100,
+              width: 100,
+              color: Colors.amber,
+              alignment: Alignment.center,
+            )
+          ],
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+        ),
+      )),
+    );
   }
 }
