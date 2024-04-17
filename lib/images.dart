b8db30fb6cca03dcdbfcfe41cadee2b30773575d
ahmedeinshouka <76920911+ahmedeinshouka@users.noreply.github.com>
2024-04-17 09:22:45 +0200

    commit

--- lib/images.dart
+++ lib/images.dart
@@ -41,7 +41,8 @@ class _newappState extends State<newapp> {
           "Face-Book",
           style: TextStyle(
               color: Color.fromARGB(255, 29, 7, 150),
-              fontWeight: FontWeight.bold,fontFamily: "LilitaOne"),
+              fontWeight: FontWeight.bold,
+              fontFamily: "LilitaOne"),
           textAlign: TextAlign.left,
         ),
         centerTitle: true,
@@ -50,12 +51,36 @@ class _newappState extends State<newapp> {
           IconButton(onPressed: () {}, icon: Icon(Icons.search))
         ],
       ),
-      body: Column(children: [
-        Text("sant_cathrine",style: TextStyle(fontSize: 35,color: Colors.yellow[700],fontFamily: "LilitaOne"),),
-           ClipRRect(child: Image.asset("assets/img/IMG_20240209_114428.jpg",width: double.infinity,fit: BoxFit.fill,height: 300,),borderRadius: BorderRadius.circular(25),),
-           
-        
-      ]),
+      body: SingleChildScrollView(
+        child: Column(children: [
+          Text(
+            "sant_cathrine",
+            style: TextStyle(
+                fontSize: 35,
+                color: Colors.yellow[700],
+                fontFamily: "LilitaOne"),
+          ),
+          ClipRRect(
+            child: Image.asset(
+              "assets/img/IMG_20240209_114428.jpg",
+              width: double.infinity,
+              fit: BoxFit.fill,
+              height: 300,
+            ),
+            borderRadius: BorderRadius.circular(25),
+          ),
+          ClipRRect(
+            child: Image.network(
+              "https://images.unsplash.com/photo-1643521935147-d4dca67b99da?q=80&w=2531&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+              width: double.infinity,
+              height: 300,
+              loadingBuilder: (context, child, loadingProgress) {
+                return loadingProgress == null? child : Container(child: CircularProgressIndicator(strokeAlign: BorderSide.strokeAlignCenter,),alignment: Alignment.center,margin: EdgeInsets.all(100),);
+              },
+            ),
+          )
+        ]),
+      ),
     );
   }
 }
