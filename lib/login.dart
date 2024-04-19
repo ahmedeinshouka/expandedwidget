200bffc97f79e6a0a5c83c6d80ad9b4221c3b5de
ahmedeinshouka <76920911+ahmedeinshouka@users.noreply.github.com>
2024-04-19 09:12:32 +0200

    2img

--- lib/login.dart
+++ lib/login.dart
@@ -23,49 +23,57 @@ class newapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(appBar: AppBar(title:Text("Guide-GO",style: TextStyle(fontFamily: "LilitaOne",fontSize: 30,color: Color.fromARGB(255, 0, 14, 121)),),centerTitle: true,elevation: .5,),
-      body: Container(
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              child: TextField(
-                cursorWidth: 2,
-                textInputAction: TextInputAction.next,
-                decoration: InputDecoration(
-                  focusColor: Colors.black,
-                
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  fillColor: const Color.fromARGB(255, 255, 255, 255),
-                hintText: "email :",prefixIcon: Icon(Icons.person)),
-              ),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(33),
-                  color: const Color.fromARGB(255, 255, 255, 255)),
-              width: 250,
-            ),SizedBox(height: 20,),Container(
-              child: TextField(obscuringCharacter: "*",obscureText: true,
-                cursorWidth: 2,
-                textInputAction: TextInputAction.next,
-                decoration: InputDecoration(
-                  focusColor: Colors.black,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  fillColor: const Color.fromARGB(255, 254, 255, 255),
-                hintText: "Password :",prefixIcon: Icon(Icons.lock)),
-              ),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(33),
-                  color: const Color.fromARGB(255, 255, 255, 255)),
-              width: 250,
-            ),SizedBox(height: 20,),
-            ElevatedButton(onPressed: (){}, child: Text("Log-in",style: TextStyle(fontFamily: "LilitaOne",fontSize: 26,color:  Color.fromARGB(255, 253, 253, 253)),),style: ButtonStyle(backgroundColor: MaterialStatePropertyAll(Color.fromARGB(255, 0, 14, 121)),fixedSize: MaterialStatePropertyAll(Size(250, 10))),)
-          ],
+      body: Stack(
+        children: [Container(width: double.infinity,
+          child: Column(
+            children: [ Container(child: Image.asset("assets/img/1.jpg"),width: double.infinity,),
+              Center(child: Container(child: Image.asset("assets/img/2.jpg"),)),
+            ],
+          ),
+        ),Container(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: TextField(
+                  cursorWidth: 2,
+                  textInputAction: TextInputAction.next,
+                  decoration: InputDecoration(
+                    focusColor: Colors.black,
+                  
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    fillColor: const Color.fromARGB(255, 255, 255, 255),
+                  hintText: "email :",prefixIcon: Icon(Icons.person)),
+                ),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(33),
+                    color: const Color.fromARGB(255, 255, 255, 255)),
+                width: 250,
+              ),SizedBox(height: 20,),Container(
+                child: TextField(obscuringCharacter: "*",obscureText: true,
+                  cursorWidth: 2,
+                  textInputAction: TextInputAction.next,
+                  decoration: InputDecoration(
+                    focusColor: Colors.black,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    fillColor: const Color.fromARGB(255, 254, 255, 255),
+                  hintText: "Password :",prefixIcon: Icon(Icons.lock)),
+                ),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(33),
+                    color: const Color.fromARGB(255, 255, 255, 255)),
+                width: 250,
+              ),SizedBox(height: 20,),
+              ElevatedButton(onPressed: (){}, child: Text("Log-in",style: TextStyle(fontFamily: "LilitaOne",fontSize: 26,color:  Color.fromARGB(255, 253, 253, 253)),),style: ButtonStyle(backgroundColor: MaterialStatePropertyAll(Color.fromARGB(255, 0, 14, 121)),fixedSize: MaterialStatePropertyAll(Size(250, 10))),)
+            ],
+          ),
         ),
-      ),backgroundColor:Color.fromARGB(255, 251, 252, 255),
+      ]),backgroundColor:Color.fromARGB(255, 251, 252, 255),
     );
   }
 }
